8f8537303b3b71be3ab03dafdb7f691f6246cf48
André Domingues <andremrmd@ua.pt>
2024-05-09 15:51:24 +0100

    Update Ball.js

--- client/src/Ball.js
+++ client/src/Ball.js
@@ -6,8 +6,8 @@ import Trace from "./Trace";
 const Ball = () => {
     const [socket, setSocket] = useState(null);
     const [players, setPlayers] = useState({});
-    const [velocity, setVelocity] = useState({ x: 20, y: 20 });
-    const [acceleration, setAcceleration] = useState({ x: 1, y: 1 });
+    const [position, setPosition] = useState({ x: 20, y: 20 });
+    const [velocity, setVelocity] = useState({ x: 1, y: 1 });
     const ballSize = 20; // Size of the ball
     const [screenWidth, setscreenWidth] = useState();
     const [screenHeight, setscreenHeight] = useState();
@@ -28,43 +28,40 @@ const Ball = () => {
         setscreenWidth(window.innerWidth);
         setscreenHeight(window.innerHeight);
 
-
-
-
         const handleKeyDown = (event) => {
             switch (event.key) {
                 case 'ArrowUp':
-                    setAcceleration(prevAcceleration => ({ ...prevAcceleration, y: prevAcceleration.y - 1 }));
+                    setVelocity(prevVelocity => ({ ...prevVelocity, y: prevVelocity.y - 1 }));
                     break;
                 case 'ArrowDown':
-                    setAcceleration(prevAcceleration => ({ ...prevAcceleration, y: prevAcceleration.y + 1 }));
+                    setVelocity(prevVelocity => ({ ...prevVelocity, y: prevVelocity.y + 1 }));
                     break;
                 case 'ArrowLeft':
-                    setAcceleration(prevAcceleration => ({ ...prevAcceleration, x: prevAcceleration.x - 1 }));
+                    setVelocity(prevVelocity => ({ ...prevVelocity, x: prevVelocity.x - 1 }));
                     break;
                 case 'ArrowRight':
-                    setAcceleration(prevAcceleration => ({ ...prevAcceleration, x: prevAcceleration.x + 1 }));
+                    setVelocity(prevVelocity => ({ ...prevVelocity, x: prevVelocity.x + 1 }));
                     break;
                 default:
                     break;
             }
         };
+
         const handleDeviceOrientation = (event) => {
             const beta = event.beta; // Angle of tilt in the front-to-back direction (-180 to 180)
             const gamma = event.gamma; // Angle of tilt in the left-to-right direction (-90 to 90)
 
-            // Calculate acceleration based on gyroscope data
-            const newAcceleration = {
+            // Calculate velocity based on gyroscope data
+            const newVelocity = {
                 x: gamma / 5, // Normalize gamma to the range -1 to 1
                 y: beta / 5 // Normalize beta to the range -1 to 1
             };
 
-            setAcceleration(newAcceleration);
+            setVelocity(newVelocity);
         };
 
         window.addEventListener('deviceorientation', handleDeviceOrientation);
         window.addEventListener('keydown', handleKeyDown);
-
 
         return () => {
             window.removeEventListener('keydown', handleKeyDown);
@@ -73,36 +70,36 @@ const Ball = () => {
 
     useEffect(() => {
         const interval = setInterval(() => {
-            // Update velocity based on acceleration
-            setVelocity(prevVelocity => ({
-                x: prevVelocity.x + acceleration.x*0.95,
-                y: prevVelocity.y + acceleration.y*0.95
+            // Update position based on velocity
+            setPosition(prevPosition => ({
+                x: prevPosition.x + velocity.x * 0.95,
+                y: prevPosition.y + velocity.y * 0.95
             }));
 
             // Emit the position of the local player's ball to the server
             if (socket) {
-                socket.emit('playerMove', { position: velocity });
+                socket.emit('playerMove', { position });
             }
         }, 1000 / 60); // Update every 16.67 milliseconds for 60 FPS
 
         return () => clearInterval(interval);
-    }, [acceleration, velocity]); // Update when acceleration or velocity changes
+    }, [velocity]); // Update when velocity changes
 
     const handleBoundaryCollision = () => {
         // Check if the ball is crossing the left or right border
-        if (velocity.x < 0 || velocity.x + ballSize > screenWidth) {
-            setAcceleration(prevAcceleration => ({ ...prevAcceleration, x: -prevAcceleration.x }));
+        if (position.x < 0 || position.x + ballSize > screenWidth) {
+            setVelocity(prevVelocity => ({ ...prevVelocity, x: -prevVelocity.x }));
         }
 
         // Check if the ball is crossing the top or bottom border
-        if (velocity.y < 0 || velocity.y + ballSize > screenHeight) {
-            setAcceleration(prevAcceleration => ({ ...prevAcceleration, y: -prevAcceleration.y }));
+        if (position.y < 0 || position.y + ballSize > screenHeight) {
+            setVelocity(prevVelocity => ({ ...prevVelocity, y: -prevVelocity.y }));
         }
     };
 
     useEffect(() => {
         handleBoundaryCollision();
-    }, [velocity, ballSize, screenWidth, screenHeight]);
+    }, [position, ballSize, screenWidth, screenHeight]);
 
     return (
         <>
@@ -120,7 +117,7 @@ const Ball = () => {
                     }}
                 ></div>
             ))}
-            <Trace position={velocity}></Trace>
+            <Trace position={position}></Trace>
             <div
                 style={{
                     width: `${ballSize}px`,
@@ -128,8 +125,8 @@ const Ball = () => {
                     borderRadius: '50%',
                     backgroundColor: 'red',
                     position: 'absolute',
-                    top: `${velocity.y}px`,
-                    left: `${velocity.x}px`,
+                    top: `${position.y}px`,
+                    left: `${position.x}px`,
                 }}
             ></div>
         </>
