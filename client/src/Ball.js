63b478ebb0060b0ba6b445178a6e73e569dbbfeb
André Domingues <andremrmd@ua.pt>
2024-06-13 12:24:01 +0100

    Update Ball.js

--- client/src/Ball.js
+++ client/src/Ball.js
@@ -7,22 +7,33 @@ const Ball = () => {
     const [players, setPlayers] = useState({});
     const [position, setPosition] = useState({ x: 20, y: 20 });
     const [velocity, setVelocity] = useState({ x: 0, y: 0 });
-    const [acceleration, setAcceleration] = useState({ x: 0, y: 0 }); // Initial acceleration is 0
-    const ballSize = 20; // Size of the ball
-
-    const calculatePlayZoneDimensions = () => {
-        const standardWidth = 1920;
-        const standardHeight = 1080;
-
-        return { playZoneWidth: standardWidth, playZoneHeight: standardHeight };
-    };
-
+    const [acceleration, setAcceleration] = useState({ x: 0, y: 0 });
     const [playZoneDimensions, setPlayZoneDimensions] = useState(calculatePlayZoneDimensions());
+
+    const ballSize = 20;
+    const playZoneAspectRatio = 1080 / 1920;
+
+    function calculatePlayZoneDimensions() {
+        const viewportWidth = window.innerWidth;
+        const viewportHeight = window.innerHeight;
+        let playZoneWidth, playZoneHeight;
+
+        if (viewportWidth / viewportHeight < playZoneAspectRatio) {
+            playZoneWidth = viewportWidth;
+            playZoneHeight = viewportWidth / playZoneAspectRatio;
+        } else {
+            playZoneHeight = viewportHeight;
+            playZoneWidth = viewportHeight * playZoneAspectRatio;
+        }
+
+        return { playZoneWidth, playZoneHeight };
+    }
 
     useEffect(() => {
         const handleResize = () => {
             setPlayZoneDimensions(calculatePlayZoneDimensions());
         };
+
         window.addEventListener('resize', handleResize);
         return () => window.removeEventListener('resize', handleResize);
     }, []);
@@ -42,16 +53,16 @@ const Ball = () => {
         const handleKeyDown = (event) => {
             switch (event.key) {
                 case 'ArrowUp':
-                    setAcceleration(prevAcceleration => ({ ...prevAcceleration, y: prevAcceleration.y - 0.1 })); // Increase acceleration upwards
+                    setAcceleration(prevAcceleration => ({ ...prevAcceleration, y: prevAcceleration.y - 0.1 }));
                     break;
                 case 'ArrowDown':
-                    setAcceleration(prevAcceleration => ({ ...prevAcceleration, y: prevAcceleration.y + 0.1 })); // Increase acceleration downwards
+                    setAcceleration(prevAcceleration => ({ ...prevAcceleration, y: prevAcceleration.y + 0.1 }));
                     break;
                 case 'ArrowLeft':
-                    setAcceleration(prevAcceleration => ({ ...prevAcceleration, x: prevAcceleration.x - 0.1 })); // Increase acceleration leftwards
+                    setAcceleration(prevAcceleration => ({ ...prevAcceleration, x: prevAcceleration.x - 0.1 }));
                     break;
                 case 'ArrowRight':
-                    setAcceleration(prevAcceleration => ({ ...prevAcceleration, x: prevAcceleration.x + 0.1 })); // Increase acceleration rightwards
+                    setAcceleration(prevAcceleration => ({ ...prevAcceleration, x: prevAcceleration.x + 0.1 }));
                     break;
                 default:
                     break;
@@ -59,16 +70,14 @@ const Ball = () => {
         };
 
         const handleDeviceOrientation = (event) => {
-            const beta = event.beta; // Angle of tilt in the front-to-back direction (-180 to 180)
-            const gamma = event.gamma; // Angle of tilt in the left-to-right direction (-90 to 90)
+            const beta = event.beta;
+            const gamma = event.gamma;
 
-            // Adjust sensitivity here
-            const sensitivity = 0.01; // Adjust this value to change sensitivity
+            const sensitivity = 0.01;
 
-            // Calculate acceleration based on gyroscope data
             const newAcceleration = {
-                x: gamma * sensitivity, // Adjust sensitivity here
-                y: beta * sensitivity // Adjust sensitivity here
+                x: gamma * sensitivity,
+                y: beta * sensitivity
             };
 
             setAcceleration(newAcceleration);
@@ -83,53 +92,43 @@ const Ball = () => {
         };
     }, []);
 
-    const maxVelocity = 10; // Set your desired maximum velocity
+    const maxVelocity = 10;
 
     useEffect(() => {
         const interval = setInterval(() => {
-            // Update velocity based on acceleration
             let newVelocity = {
                 x: velocity.x + acceleration.x,
                 y: velocity.y + acceleration.y
             };
 
-            // Clamp velocity to stay within the maximum allowed value
             newVelocity.x = Math.min(Math.max(newVelocity.x, -maxVelocity), maxVelocity);
             newVelocity.y = Math.min(Math.max(newVelocity.y, -maxVelocity), maxVelocity);
 
             setVelocity(newVelocity);
 
-            // Update position based on velocity
             setPosition(prevPosition => ({
                 x: prevPosition.x + newVelocity.x * 0.95,
                 y: prevPosition.y + newVelocity.y * 0.95
             }));
 
-            // Emit the position of the local player's ball to the server
             if (socket) {
                 socket.emit('playerMove', { position });
             }
-        }, 1000 / 60); // Update every 16.67 milliseconds for 60 FPS
+        }, 1000 / 60);
 
         return () => clearInterval(interval);
-    }, [acceleration, velocity, socket]); // Update when acceleration or velocity changes
+    }, [acceleration, velocity, socket]);
 
     useEffect(() => {
         handleBoundaryCollision(playZoneDimensions.playZoneWidth, playZoneDimensions.playZoneHeight);
     }, [position, ballSize, playZoneDimensions]);
 
     const handleBoundaryCollision = (playZoneWidth, playZoneHeight) => {
-        const scaleX = window.innerWidth / playZoneDimensions.playZoneWidth;
-        const scaleY = window.innerHeight / playZoneDimensions.playZoneHeight;
-        const scale = Math.min(scaleX, scaleY);
-
-        // Check if the ball is crossing the left or right border
-        if (position.x * scale < 0 || position.x * scale + ballSize > playZoneWidth) {
+        if (position.x < 0 || position.x + ballSize > playZoneWidth) {
             setVelocity(prevVelocity => ({ ...prevVelocity, x: -prevVelocity.x }));
         }
 
-        // Check if the ball is crossing the top or bottom border
-        if (position.y * scale < 0 || position.y * scale + ballSize > playZoneHeight) {
+        if (position.y < 0 || position.y + ballSize > playZoneHeight) {
             setVelocity(prevVelocity => ({ ...prevVelocity, y: -prevVelocity.y }));
         }
     };
@@ -150,7 +149,7 @@ const Ball = () => {
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
-            overflow: 'hidden',  // Prevent scrolling
+            overflow: 'hidden',
             position: 'relative'
         }}>
             <div style={playZoneStyle}>
@@ -161,10 +160,10 @@ const Ball = () => {
                             width: `${ballSize}px`,
                             height: `${ballSize}px`,
                             borderRadius: '50%',
-                            backgroundColor: 'darkolivegreen', // Change color for other players' balls
+                            backgroundColor: 'darkolivegreen',
                             position: 'absolute',
-                            top: `${players[playerId].y * scale}px`,
-                            left: `${players[playerId].x * scale}px`,
+                            top: `${players[playerId].y}px`,
+                            left: `${players[playerId].x}px`,
                         }}
                     ></div>
                 ))}
@@ -176,8 +175,8 @@ const Ball = () => {
                         borderRadius: '50%',
                         backgroundColor: 'red',
                         position: 'absolute',
-                        top: `${position.y * scale}px`,
-                        left: `${position.x * scale}px`,
+                        top: `${position.y}px`,
+                        left: `${position.x}px`,
                     }}
                 ></div>
             </div>
